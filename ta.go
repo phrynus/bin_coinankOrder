38abe9f19d3ab63295f1a2b47f9950d51c3a709b
phrtynus <fondantcn@gmail.com>
2024-11-04 18:47:06 +0800

    c

--- ta.go
+++ ta.go
@@ -47,3 +47,38 @@ func RSI(ohlc []float64, period int) []float64 {
 
 	return rsi
 }
+
+func ATR(ohlc [][]float64, period int) []float64 {
+	if len(ohlc) < period {
+		return nil
+	}
+
+	tr := make([]float64, len(ohlc))
+	atr := make([]float64, len(ohlc))
+
+	// 计算 RT
+	for i := 1; i < len(ohlc); i++ {
+		high := ohlc[i][2]
+		low := ohlc[i][3]
+		prevClose := ohlc[i-1][4]
+
+		tr1 := high - low
+		tr2 := math.Abs(high - prevClose)
+		tr3 := math.Abs(low - prevClose)
+		tr[i] = math.Max(tr1, math.Max(tr2, tr3))
+	}
+
+	// 初始化 ATR 为前 period 天的 TR 平均值
+	var sumTR float64
+	for i := 1; i <= period; i++ {
+		sumTR += tr[i]
+	}
+	atr[period] = sumTR / float64(period)
+
+	// 计算之后的 ATR 值
+	for i := period + 1; i < len(ohlc); i++ {
+		atr[i] = (atr[i-1]*(float64(period)-1) + tr[i]) / float64(period)
+	}
+
+	return atr
+}
