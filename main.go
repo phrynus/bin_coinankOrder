9c6135a4474ce353bf917b453ce28c7c66e9da28
phrtynus <fondantcn@gmail.com>
2024-11-06 18:23:47 +0800

    min

--- main.go
+++ main.go
@@ -358,22 +358,22 @@ func filterSymbols(symbols []FundData) ([]FundData, error) {
 		crsi := CRSI(closedPrices, config.RsiLength)
 		// fmt.Println(s.Coin, s.Side, closedPrices[200], crsi[200])
 		if s.Side && crsi[200] < config.RsiLevel {
-			log.Println("["+s.Coin+"] [多 RSI] | ", closedPrices[200], " RSI:", crsi[200], s)
+			log.Println("[ "+s.Coin+" ] [多] [ RSI ] | 价格:", closedPrices[200], " RSI:", crsi[200], " Coinan:", s)
 			target = append(target, s)
 			continue
 		}
 		if !s.Side && crsi[200] > (100-config.RsiLevel) {
-			log.Println("["+s.Coin+"] [空 RSI] | ", closedPrices[200], " RSI:", crsi[200], s)
+			log.Println("[ "+s.Coin+" ] [空] [ RSI ] | 价格:", closedPrices[200], " RSI:", crsi[200], " Coinan:", s)
 			target = append(target, s)
 			continue
 		}
 		if s.Side && s.M5Net > config.BuyNetAmount && s.M15Net > 1 && s.M15Net > (s.M5Net*config.MultipleNetAmount) {
-			log.Println("["+s.Coin+"] [多 量级] | ", closedPrices[200], " RSI:", crsi[200], s)
+			log.Println("[ "+s.Coin+" ] [多] [ 量级 ] | 价格:", closedPrices[200], " RSI:", crsi[200], " Coinan:", s)
 			target = append(target, s)
 			continue
 		}
 		if !s.Side && s.M5Net < -config.SideNetAmount && s.M15Net < 1 && s.M15Net < (s.M5Net*config.MultipleNetAmount) {
-			log.Println("["+s.Coin+"] [空 量级] | ", closedPrices[200], " RSI:", crsi[200], s)
+			log.Println("[ "+s.Coin+" ] [空] [ 量级 ] | 价格:", closedPrices[200], " RSI:", crsi[200], " Coinan:", s)
 			target = append(target, s)
 			continue
 		}
